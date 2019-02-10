4bcc60521a838c3949a72d34a9b346d8a8aa7d2e
Lauren Hetherington <hetherington@astro.utoronto.ca>
2019-02-10 13:04:45 -0500

    Added a click-for-trains button.

---
+++ Assets/Scripts/TrainDepot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TrainDepot : MonoBehaviour, IPointerClickHandler {
+    public GameObject trainPrefab;
+    public float trainInterval;
+    float nextTrainTime;
+	// Use this for initialization
+	void Start () {
+        nextTrainTime = Time.time + trainInterval;
+	}
+	
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (Time.time>nextTrainTime) {
+            nextTrainTime = Time.time + trainInterval;
+            Instantiate(trainPrefab, null);
+        }
+    }
+}
